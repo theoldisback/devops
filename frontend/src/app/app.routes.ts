a4d515e2c5744bcad118f74cb48403968368229f
mohamed_abdelkebir <mohamed.abdelkebir1@esprit.tn>
2024-11-06 07:54:29 +0100

    frontend

--- frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { RoomComponent } from './room/room.component';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-    {path: 'room', component: RoomComponent},
-    {path: 'home', component: HomeComponent},
-];
